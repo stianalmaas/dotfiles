ed8f6fa03a2f7e2598f56a09addce6d20310f4ea
Stian Almås <stian.almaas@gmail.com>
2012-10-12 22:40:46 +0200

    med el-get

--- .emacs.d/init.d/01_packages.el
+++ .emacs.d/init.d/01_packages.el
@@ -1,46 +1,58 @@
 (require 'package)
 (add-to-list 'package-archives 
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-	     '("ELPA" . "http://tromey.com/elpa/"))
+ (add-to-list 'package-archives
+ 	     '("tromey" . "http://tromey.com/elpa/"))
 (package-initialize)
 
-(when (null package-archive-contents)
-  (package-refresh-contents))
+;; (when (null package-archive-contents)
+;;   (package-refresh-contents))
 
-(defvar my-packages '(starter-kit 
-                      starter-kit-lisp 
-                      starter-kit-bindings
-                      starter-kit-eshell 
-                      zenburn-theme
-                      clojure-mode 
-                      clojure-test-mode
-                      markdown-mode 
-                      yaml-mode
-                      tuareg 
-                      haskell-mode
-                      marmalade 
-                      oddmuse 
-                      scpaste))
+;; (defvar my-packages '(starter-kit 
+;;                       starter-kit-lisp 
+;;                       starter-kit-bindings
+;;                       starter-kit-eshell 
+;;                       zenburn-theme
+;;                       clojure-mode 
+;;                       clojure-test-mode
+;;                       markdown-mode 
+;;                       yaml-mode
+;;                       tuareg 
+;;                       haskell-mode
+;;                       marmalade 
+;;                       oddmuse 
+;;                       scpaste))
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+;; (dolist (p my-packages)
+;;   (when (not (package-installed-p p))
+;;     (package-install p)))
 
 
 
 ;; el-get - see https://github.com/dimitri/el-get
-; (setq el-get-dir (expand-file-name "el-get" user-emacs-directory))
+(setq el-get-dir (expand-file-name "el-get" user-emacs-directory))
 
-;(add-to-list 'load-path 
-;             (expand-file-name "el-get/el-get" user-emacs-directory))
-;(unless (require 'el-get nil t) 
-;  (url-retrieve "https://raw.github.com/dimitri/el-get/master/el-get-install.el" 
-;                (lambda (s) (end-of-buffer) (eval-print-last-sexp))))
+(add-to-list 'load-path 
+             (expand-file-name "el-get/el-get" user-emacs-directory))
+(setq el-get-user-package-directory "~/.emacs.d/packages.d/")
+
+(unless (require 'el-get nil t)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(el-get 'sync)
+
+;; (unless (require 'el-get nil t) 
+;;   (url-retrieve "https://raw.github.com/dimitri/el-get/master/el-get-install.el" 
+;;                 (lambda (s) (end-of-buffer) (eval-print-last-sexp))))
 
 ;; local sources
-;(setq el-get-sources
-;      '((:name asciidoc
+(setq el-get-sources
+      '(
+;(:name asciidoc
 ;	       :type elpa
 ;	       :after (lambda ()
 ;			(autoload 'doc-mode "doc-mode" nil t)
@@ -59,20 +71,21 @@
 ;        (:name magit-gh-pulls :type elpa)        
 					;	(:name eieio :type elpa)
 ;	(:name gist :type elpa)
-;	(:name starter-kit :type marmalade)
-;	(:name starter-kit-elisp :type elpa)
-;	(:name starter-kit-bindings :type elpa)
-;       (:name starter-kit-eshell :type elpa)
+	(:name starter-kit :type marmalade)
+	(:name starter-kit-elisp :type marmalade)
+	(:name starter-kit-bindings :type marmalade)
+        (:name starter-kit-eshell :type marmalade)
 ;	(:name clojure-mode :type elpa)
 ;	(:name clojure-test-mode :type elpa)
 	
 ;	(:name magit				; git meet emacs, and a binding
 ;	     :after (progn
 ;		      (global-set-key (kbd "C-x C-z") 'magit-status)))
-;	))
+                                        ;	)
+        ))
 
-;(setq sla-packages
-;      '(
+(setq sla-packages
+      '(
 	; eieio
 					; xml-rpc
 					; gh
@@ -102,7 +115,7 @@
 					; minimap
 					; multi-term
 					; scss-mode
-;	))
+	))
 
 ;; Annoying packages that explode during install if their deps are not met
 ;(setq sla-dependent-packages
