c0646c51a9a14e9aff67cef1294889cfe82e2a76
Stian Almås <stian.almaas@gmail.com>
2013-12-18 21:42:38 +0100

    Remapping keys on Mac

---
+++ .emacs.d/osx.el
@@ -0,0 +1,16 @@
+;; change command to meta, and ignore option to use weird Norwegian keyboard
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+(setq ns-function-modifier 'hyper)
+
+;; Norwegian mac-keyboard alt-keys)
+(define-key key-translation-map (kbd "s-8") (kbd "["))
+(define-key key-translation-map (kbd "s-(") (kbd "{"))
+(define-key key-translation-map (kbd "s-9") (kbd "]"))
+(define-key key-translation-map (kbd "s-)") (kbd "}"))
+(define-key key-translation-map (kbd "s-7") (kbd "|"))
+(define-key key-translation-map (kbd "s-/") (kbd "\\"))
+(define-key key-translation-map (kbd "M-s-7") (kbd "M-|"))
+
+(global-set-key (kbd "s-u") 'universal-argument)
+(global-set-key (kbd "s--") 'negative-argument)
